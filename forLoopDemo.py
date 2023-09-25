ee4bfbefc94379349835802ddd57883110e7a271
username016 <mohammed@sherkhan.ca>
2023-09-25 14:19:43 -0400

    First commit

--- forLoopDemo.py
+++ forLoopDemo.py
@@ -1,2 +1,8 @@
 for num in range(10,20): #The upper bound is excluded (it will not print the number 20)
     print(num)
+    
+for num in range(20,10,-2): #The upper bound is excluded (it will not print the number 20)
+    print(num)
+    
+for num in range(10):
+    print(num)
