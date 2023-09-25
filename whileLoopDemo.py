7b99a60c1dd0fef3dd6dd41fb2658ef66ca052ca
username016 <mohammed@sherkhan.ca>
2023-09-25 15:36:19 -0400

    Understanding indexing

--- whileLoopDemo.py
+++ whileLoopDemo.py
@@ -1,6 +1,39 @@
-numbers = [12,23,34,45]
-index = 0
-while index < len(numbers):
-    index += 2 # same as index = index + 1 
-    print(numbers[index])
+numbers = [12,23,-1,34,45]
+ind = 0
+
+while ind < len(numbers):
+    print(numbers[ind])
+    ind += 1 # same as ind = ind + 1
+
+
+# This is how to find the negative number inside the list using the while loop when it encounter a negative number 
+
+ind = 0
+while numbers[ind] > 0:
+    print(numbers[ind])
+    ind += 1
+    
+print("hello")
+    
+    
+ # My attempt
+#while numbers[ind]:
+ #    ind += 1 #same as index = index + 1
+  #   if (numbers[ind] > 0):
+   #         print(numbers[ind])   
+    
+
+
+
+    
+numbering = [12,23,10,34,45]
+index = 0 
+while index < len(numbering) and numbering[index] > 0:
+    print(numbering[index])
+    index += 1
+
+
+
+   
+    
     
