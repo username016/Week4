ad093aeb1c52b4597682193d17fcc6723f0c17b2
username016 <mohammed@sherkhan.ca>
2023-09-25 14:21:34 -0400

    Demonstarting the loops at work

--- forLoopDemo.py
+++ forLoopDemo.py
@@ -1,7 +1,7 @@
 for num in range(10,20): #The upper bound is excluded (it will not print the number 20)
     print(num)
     
-for num in range(20,10,-2): #The upper bound is excluded (it will not print the number 20)
+for num in range(20,10,-1): #The upper bound is excluded (it will not print the number 20) and it would decrement from the third value in this case -1 
     print(num)
     
 for num in range(10):
